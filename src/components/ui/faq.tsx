9550aee09e5c2de7a197859e08b45610b3731306
Justin Singh <justin.singh125125@gmail.com>
2025-03-15 11:21:39 -0700

    updated FAQ

--- src/components/ui/faq.tsx
+++ src/components/ui/faq.tsx
@@ -6,46 +6,59 @@ type FAQItem = { id: number; question: string; answer: string };
 const faqs: FAQItem[] = [
   {
     id: 1,
-    question: "Are children allowed?",
-    answer: "Yes, we would love to invite your children to our wedding.",
+    question: "Will children be allowed?",
+    answer:
+      "Yes, we would love to invite your children to our wedding. If your invite doesn’t have the option to include your loved ones, please let us know.",
   },
   {
     id: 2,
-    question: "Indoor or Outdoor?",
+    question: "Will it be an indoor or outdoor wedding?",
     answer: "We will be outside for the whole event.",
   },
   {
     id: 3,
-    question: "What is the dress code?",
-    answer:
-      "Please come in your best formal attire. We will be in our September weather so please keep in mind the outdoor heat.",
+    question: "Will there be different locations?",
+    answer: "There is only one location, 9440 Bar Du Ln Sacramento, CA 95829.",
   },
   {
     id: 4,
-    question: "Will there be gifts for guests?",
-    answer: "Yes.",
+    question: "Will I have to pay for parking?",
+    answer:
+      "We will have parking on site for free and it will be on the property close by.",
   },
   {
     id: 5,
+    question: "What is the dress code?",
+    answer:
+      "Please come in your best formal attire. We will be in our September weather so please keep in mind of the outdoor heat.",
+  },
+  {
+    id: 6,
+    question: "Can I take photos?",
+    answer:
+      "Our ceremony will be an unplugged ceremony, but after please take as many photos as you’d like. We will also have a QR code for you to share your photos with us.",
+  },
+  {
+    id: 7,
     question: "Will there be bathrooms?",
     answer: "Yes, the bathrooms will be inside along side the bar.",
   },
   {
-    id: 6,
+    id: 8,
     question: "Will there be alcohol?",
     answer:
       "Alcohol will be available, although no hard alcohol will be allowed. If hard alcohol is caught on the property, the individual(s) will be requested to leave.",
   },
   {
-    id: 7,
+    id: 9,
     question: "Will it be an open bar?",
     answer: "Guests will have to pay for their own alcohol.",
   },
   {
-    id: 8,
+    id: 10,
     question: "What will the food be like?",
     answer:
-      "We will be catered by Adoboink, a modern/traditional Filipino restaurant.",
+      "We will be catered by Adoboink, a modern/traditional Filipino restaurant. It will be served buffet style and each table will be released to take a photo with the newlyweds before grabbing a plate.",
   },
 ];
 
