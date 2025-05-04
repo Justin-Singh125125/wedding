9fd7f45363ba87d2b6c64fa1f9cc3807992001d9
Justin Singh <justin.singh125125@gmail.com>
2025-05-04 13:58:35 -0700

    updated registry section

--- src/components/registry-section.tsx
+++ src/components/registry-section.tsx
@@ -1,33 +1,48 @@
-import React from "react";
-import { ExternalLink } from "lucide-react";
+import type React from "react";
+import { ExternalLink, Heart, Home, ShoppingCart } from "lucide-react";
 
-type RegistryItem = { id: number; label: string; url: string };
+type RegistryItem = {
+  id: number;
+  label: string;
+  url: string;
+  icon: React.ReactNode;
+};
 
 const registryItems: RegistryItem[] = [
+  {
+    id: 2,
+    label: "Honeymoon",
+    url: "https://account.venmo.com/u/jjgetsmarried",
+    icon: <Heart size={24} />,
+  },
   {
     id: 1,
     label: "Crate And Barrel",
     url: "https://www.crateandbarrel.com/gift-registry/justin%20and%20jenna-singh/r7283902",
+    icon: <Home size={24} />,
   },
   {
-    id: 2,
+    id: 3,
     label: "Amazon",
     url: "https://www.amazon.com/wedding/share/jjgetsmarried",
+    icon: <ShoppingCart size={24} />,
   },
-  { id: 2, label: "Venmo", url: "https://account.venmo.com/u/jjgetsmarried" },
 ];
 
-type RegistryItemProps = Pick<RegistryItem, "label" | "url">;
+type RegistryItemProps = Pick<RegistryItem, "label" | "url" | "icon">;
 
-const RegistryItem = ({ label, url }: RegistryItemProps) => {
+const RegistryItem = ({ label, url, icon }: RegistryItemProps) => {
   return (
     <a
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex w-full flex-1 items-center justify-between rounded-md bg-white p-4 shadow-md hover:fill-orange-400 hover:text-primary-400"
+      className="group flex w-full flex-1 items-center justify-between rounded-md bg-white p-4 shadow-md hover:fill-orange-400 hover:text-primary-400"
     >
-      <span>{label}</span>
+      <div className="flex items-center gap-3">
+        <span className="group-hover:text-primary-400">{icon}</span>
+        <span>{label}</span>
+      </div>
       <ExternalLink size={28} />
     </a>
   );
@@ -39,7 +54,12 @@ export function RegistrySection() {
       <h2 className="text-center">Registry</h2>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         {registryItems.map((item) => (
-          <RegistryItem key={item.id} label={item.label} url={item.url} />
+          <RegistryItem
+            key={item.id}
+            label={item.label}
+            url={item.url}
+            icon={item.icon}
+          />
         ))}
       </div>
     </section>
